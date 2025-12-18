9cd73ebff01e572bab12f0767e78e407b0bcc90d
José Cabeda <jecabeda@gmail.com>
2025-12-18 23:52:22 +0000

    feat: refactor database initialization to use PrismaBetterSqlite3 directly

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
-import Database from 'better-sqlite3';
 import path from 'path';
 import fs from 'fs';
 
@@ -13,8 +12,7 @@ if (!fs.existsSync(dataDir)) {
 const dbPath = path.join(dataDir, 'stopwatch.db');
 console.log('Initializing Prisma with better-sqlite3 at:', dbPath);
 
-const db = new Database(dbPath);
-const adapter = new PrismaBetterSqlite3(db);
+const adapter = new PrismaBetterSqlite3({ url: dbPath });
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
